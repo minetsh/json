16448d7e34c9cfa45e96d69cc0c3ffaace2e0210
Felix <felix@xingren.com>
2020-04-19 01:04:49 +0800

    fix: 修复路径问题

---
+++ precache-manifest.b9db8f1f3964203d36ca3aa56d0c0fee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14d02cb6c93c17fa1307dd9d8d0f7c8",
+    "url": "/json/index.html"
+  },
+  {
+    "revision": "e07037612a61eb6f8da9",
+    "url": "/json/static/css/main.e89a0ee3.chunk.css"
+  },
+  {
+    "revision": "23f88c09eac2ede581fc",
+    "url": "/json/static/js/2.60715e04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/json/static/js/2.60715e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07037612a61eb6f8da9",
+    "url": "/json/static/js/main.b3d1affd.chunk.js"
+  },
+  {
+    "revision": "ebe66ca109563f1114b0",
+    "url": "/json/static/js/runtime-main.6cff4b0a.js"
+  },
+  {
+    "revision": "a60b33758e5f1401165c6e18cf576266",
+    "url": "/json/static/media/wechat.a60b3375.jpeg"
+  }
+]);

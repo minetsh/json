1fe92925cb50ae6a01bf25f7c5d6e020e1c60531
Felix <felix@xingren.com>
2020-04-19 00:58:29 +0800

    feat: 发布 v0.1.0

---
+++ build/precache-manifest.95cc3b8ba05056b7f063bf131bdee64c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec78b7597bbd623edbe3aab3d744721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dba7d4b14475dbcda5a2",
+    "url": "/static/css/main.e89a0ee3.chunk.css"
+  },
+  {
+    "revision": "23f88c09eac2ede581fc",
+    "url": "/static/js/2.60715e04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60715e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba7d4b14475dbcda5a2",
+    "url": "/static/js/main.67e7fb8c.chunk.js"
+  },
+  {
+    "revision": "c27594c53b065ee160e2",
+    "url": "/static/js/runtime-main.6258e7a6.js"
+  },
+  {
+    "revision": "a60b33758e5f1401165c6e18cf576266",
+    "url": "/static/media/wechat.a60b3375.jpeg"
+  }
+]);
